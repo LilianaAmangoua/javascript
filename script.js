7cbf0bd72df2c02780c86cf150b7b6bd25e7db02
LilianaAmangoua <amangoua99@gmail.com>
2024-11-29 07:38:27 +0100

    Exercise 1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+let section = document.getElementById("hero-section");
+let newDiv = document.createElement("div");
+section.appendChild(newDiv); 
+
+let newTitle = document.createElement("h3");
+newDiv.appendChild(newTitle);
+newTitle.id = "myTitle";
+newTitle.innerText = "Voici le titre";
+
+let newParagraph = document.createElement("p");
+newDiv.appendChild(newParagraph);
+newParagraph.innerText = "Ceci est un paragraphe";
+
+let newButton = document.createElement("button");
+newDiv.appendChild(newButton);
+newButton.classList = "myButton";
+newButton.innerText = "Button";
